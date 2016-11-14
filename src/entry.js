ff1da061ec879c2002d1913e538c6e2e2c40dac5
Artur_Dubrovin <artur_dubrovin@epam.com>
2016-11-14 13:54:58 +0300

    Update 2016-11-14T10:54:52.310Z

--- src/entry.js
+++
@@ -1,9 +0,0 @@
-require('smoothscroll-polyfill').polyfill();
-
-document.querySelectorAll('.nav__item').addEventListener('click', scroll);
-
-function scroll() {
-  this.scrollIntoView({
-    behavior: 'smooth'
-  });
-}
