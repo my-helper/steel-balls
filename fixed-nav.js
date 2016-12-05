03e289e13a416e912b837e8187ad727260b4c856
Artur_Dubrovin <artur_dubrovin@epam.com>
2016-12-05 13:46:41 +0300

    Update 2016-12-05T10:46:31.804Z

--- fixed-nav.js
+++ fixed-nav.js
@@ -4,19 +4,30 @@ function fixedNav() {
   var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
       _ref$linkEl = _ref.linkEl,
       linkEl = _ref$linkEl === undefined ? document.querySelectorAll('.nav__item') : _ref$linkEl,
-      _ref$offset = _ref.offset,
-      offset = _ref$offset === undefined ? 39 : _ref$offset;
+      _ref$navEl = _ref.navEl,
+      navEl = _ref$navEl === undefined ? document.querySelector('.header') : _ref$navEl,
+      _ref$bodyEl = _ref.bodyEl,
+      bodyEl = _ref$bodyEl === undefined ? document.querySelector('.under-fixed') : _ref$bodyEl;
 
-  var linkLength = linkEl.length;
+  var linkLength = linkEl.length,
+      navLength = navEl.offsetHeight;
 
   for (var i = 0; i < linkLength; i++) {
     linkEl[i].addEventListener('click', function (e) {
       e.preventDefault();
-      var id = this.getAttribute('href').substring(1);
-      var el = document.getElementById(id);
-      el.scrollIntoView();
-      window.scrollTo(0, window.scrollY - offset);
+      anchorClick(this, navLength);
       toggleEl(navContainer, 'hidden-sm-down');
     });
   }
+
+  // console.log(navEl.offsetHeight);
+  function addMarginTop(addTo, getFrom) {
+    addTo.style.marginTop = getFrom.offsetHeight + 'px';
+  }
+
+  addMarginTop(bodyEl, navEl);
+
+  window.onresize = function () {
+    addMarginTop(bodyEl, navEl);
+  };
 }
