f230d7533b871b9f10cfd9cea32b1d04307d83f8
my-helper <get.it@zoho.com>
2016-12-06 12:45:28 +0300

    burger icon animation

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,10 +1,14 @@
-let navBtn = document.querySelector('.nav-btn');
-let nav = document.querySelector('.nav');
-let navContainer = nav.parentElement.parentElement;
+const navBtn = document.querySelector('.nav-btn'),
+  nav = document.querySelector('.nav'),
+  navContainer = nav.parentElement.parentElement,
+  navBtnLines = document.querySelectorAll('.nav-btn__lines');
 
 navBtn.addEventListener('click', function (e) {
   e.preventDefault();
   toggleEl(navContainer, 'hidden-sm-down');
+  for( let el of navBtnLines) {
+    toggleEl(el, 'nav-btn__lines_open');
+  }
 });
 
 function toggleEl(el, cssClass) {
